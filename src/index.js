80330f8e2e32b5f98c2bffe8cb28a7f6e3baf011
Sergey Mochalov <svmochalov@kpfu.ru>
2025-11-30 14:49:25 +0300

    Update index.js
    Update main script

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import { createRoot } from '@wordpress/element';
-
-
-import { Panel, PanelBody, PanelRow, Button, Icon } from '@wordpress/components';
-import { __experimentalHeading as Heading } from '@wordpress/components';
-import { verse, plusCircle, external, commentAuthorAvatar } from '@wordpress/icons';
-
+import { Panel, PanelBody, PanelRow, Button } from "@wordpress/components";
+import { __experimentalHeading as Heading } from "@wordpress/components";
+import { verse, plusCircle, external } from "@wordpress/icons";
 
 
 const rootElement = document.getElementById('demo-app');
@@ -12,51 +9,51 @@ const rootElement = document.getElementById('demo-app');
 /**
  * App component renders a demonstration panel using React components
  * from WordPress. It includes two PanelBody sections:
- * 
+ *
  * 1. The first PanelBody provides a heading and a description paragraph
  *    with an icon, showcasing the use of React components for interface
  *    development.
- * 
+ *
  * 2. The second PanelBody displays various styled buttons, demonstrating
  *    different button variants available in WordPress components. Each
  *    button has specific styling and some have icons with specific positions.
  */
 
 const App = () => (
-    <>
-        <Panel header="React in WordPress">
-            <PanelBody title="Title for PanelBody" icon={verse} initialOpen={true}>
-                <Heading level={2}>Компоненты на React для разработки интерфейса</Heading>
-                <PanelRow>
-                    <p>демо-плагин. Создано c учебными целями...</p>
+	<>
+		<Panel header="Hello! React in WordPress">
+			<PanelBody title="Title for PanelBody" icon={verse} initialOpen={true}>
+				<Heading level={2}>
+					Компоненты на React для разработки интерфейса
+				</Heading>
+				<PanelRow>
+					<p>демо-плагин. Создано c учебными целями...</p>
+				</PanelRow>
+				<PanelRow>
+					<>
+						<Heading level={4}>разработка - Сергей Мочалов</Heading>
+					</>
+				</PanelRow>
+			</PanelBody>
 
-                </PanelRow>
-                <PanelRow>
-                     <>
-                    <Heading level={4}>разработка - Сергей Мочалов</Heading>  
-                    </>
-                </PanelRow>
-
-            </PanelBody>
-
-
-            <PanelBody title="Компоненты: Buttons" initialOpen={true}>
-                <Heading level={3}>Стили кнопок:</Heading>
-                <PanelRow>
-                    <Button icon={plusCircle} iconPosition='left'>Defaut</Button>
-                    <Button variant='primary' >Primary</Button>
-                    <Button variant='secondary' onClick={() => alert('Secondary')}>Secondary</Button>
-                    <Button variant='tertiary'>Tertiary</Button>
-                    <Button variant='link' icon={external} iconPosition='right'>Link</Button>
-                </PanelRow>
-                <PanelRow></PanelRow>
-                <PanelRow></PanelRow>
-                <PanelRow></PanelRow>
-
-            </PanelBody>
-        </Panel>
-
-    </>
+			<PanelBody title="Компоненты: Buttons" initialOpen={true}>
+				<Heading level={3}>Стили кнопок:</Heading>
+				<PanelRow>
+					<Button icon={plusCircle} iconPosition="left">
+						Defaut
+					</Button>
+					<Button variant="primary">Primary</Button>
+					<Button variant="secondary" onClick={() => alert("Secondary")}>
+						Secondary
+					</Button>
+					<Button variant="tertiary">Tertiary</Button>
+					<Button variant="link" icon={external} iconPosition="right">
+						Link
+					</Button>
+				</PanelRow>
+			</PanelBody>
+		</Panel>
+	</>
 );
 
 if (rootElement) {
