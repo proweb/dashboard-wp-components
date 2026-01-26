8d25e9d5913eebfe6d9ef3f1896fb596bd8e4d69
Sergey Mochalov <svmochalov@kpfu.ru>
2026-01-26 11:57:26 +0300

    chore: remove accidental dashboard-wp-components folder

--- dashboard-wp-components/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'ea09d4490555b81bc8f1');
--- dashboard-wp-components/src/components/ui/Button.js
+++
@@ -1,3 +0,0 @@
-import { Button } from '@wordpress/components';
-
-export const Mybutton = () => <Button variant="primary">Save</Button>;
