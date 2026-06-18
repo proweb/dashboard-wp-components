5a655393575bd49f8444f46c427cd4e69d236eb1
Мочалов Сергей Вячеславович <680073+proweb@users.noreply.github.com>
2026-06-18 13:25:45 +0300

    update build

--- build/components.asset.php
+++ build/components.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '826872cb6d02c657b280');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd81e55681ad8306c9f18');
